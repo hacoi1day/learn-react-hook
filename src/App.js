426659a62d860453f160e3dfdac72de8f0f06dde
havt <vuha98.tn@gmail.com>
2021-02-01 15:58:44 +0700

    use effect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,70 @@
 import './App.scss';
-import ColorBox from "./components/ColorBox";
+import {useState, useEffect} from "react";
+import TodoList from "./components/TodoList";
+import TodoForm from "./components/TodoForm";
+import PostList from "./components/PostList";
 
 function App() {
+
+  const [todoList, setTodoList] = useState([
+    {id: 1, title: 'Learn English'},
+    {id: 2, title: 'Learn React'},
+    {id: 3, title: 'Learn Angular'},
+  ]);
+
+  const [postList, setPostList] = useState([])
+
+  useEffect(() => {
+    async function fetchPostList() {
+      try {
+        const requestUrl = 'http://js-post-api.herokuapp.com/api/posts?_limit=10&_page=1';
+        const response = await fetch(requestUrl);
+        const responseJSON = await response.json();
+
+        const {data} = responseJSON;
+        setPostList(data);
+      } catch (error) {
+        console.log('Error to fetch post lists: ', error.message);
+      }
+    }
+    console.log('POST list effect');
+    fetchPostList();
+  }, []);
+
+  useEffect(() => {
+    console.log('TODO list effect');
+  })
+
+  function handleTodoClick(todo) {
+    const index = todoList.findIndex(x => x.id === todo.id);
+    if (index < 0) return;
+    const newTodoList = [...todoList];
+    newTodoList.splice(index, 1);
+    setTodoList(newTodoList);
+  }
+
+  function handleTodoFormSubmit(formValues) {
+    // console.log(formValues);
+    const newTodo = {
+      id: todoList.length + 1,
+      ...formValues
+    };
+    const newTodoList = [...todoList];
+    newTodoList.push(newTodo);
+    setTodoList(newTodoList);
+  }
+
+
   return (
     <div className="app">
-      <h1>Welcome to React Hook</h1>
-      <ColorBox/>
+      <h1>React Hook - PostList</h1>
+
+      <TodoForm onSubmit={handleTodoFormSubmit}/>
+      <TodoList todos={todoList} onTodoClick={handleTodoClick}/>
+
+      <hr/>
+      <PostList posts={postList} />
+
     </div>
   );
 }
---
+++ src/components/TodoForm/index.jsx
@@ -0,0 +1,48 @@
+import React, {useState} from 'react';
+import PropTypes from 'prop-types';
+
+TodoForm.propTypes = {
+  onSubmit: PropTypes.func,
+};
+
+TodoForm.defaultProps = {
+  onSubmit: null
+};
+
+function TodoForm(props) {
+
+  const {onSubmit} = props;
+
+  const [value, setValue] = useState('');
+
+  function handleValueChange(e) {
+    // console.log(e.target.value);
+    setValue(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    // console.log(value);
+    if (!onSubmit) return;
+
+    const formValue = {
+      title: value
+    }
+    onSubmit(formValue);
+
+    // Reset form
+    setValue('');
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={value}
+        onChange={handleValueChange}
+      />
+    </form>
+  );
+}
+
+export default TodoForm;

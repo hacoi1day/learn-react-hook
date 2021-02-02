6ac4cc421f702e183c10f6701080d805640c2cc5
havt <vuha98.tn@gmail.com>
2021-02-02 14:39:28 +0700

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.scss';
 import {useState, useEffect} from "react";
+import queryString from "query-string";
 import TodoList from "./components/TodoList";
 import TodoForm from "./components/TodoForm";
 import PostList from "./components/PostList";
+import Pagination from "./components/Pagination";
+import PostFilterForm from "./components/PostFilterForm";
+
 
 function App() {
 
@@ -13,27 +17,47 @@ function App() {
   ]);
 
   const [postList, setPostList] = useState([])
+  const [pagination, setPagination] = useState({
+    _page: 1,
+    _limit: 10,
+    _totalRows: 1,
+  });
+
+  const [filter, setFilter] = useState({
+    _limit: 10,
+    _page: 1,
+    title_like: '',
+  });
 
   useEffect(() => {
     async function fetchPostList() {
       try {
-        const requestUrl = 'http://js-post-api.herokuapp.com/api/posts?_limit=10&_page=1';
+        const paramString = queryString.stringify(filter);
+        const requestUrl = `http://js-post-api.herokuapp.com/api/posts?${paramString}`;
         const response = await fetch(requestUrl);
         const responseJSON = await response.json();
 
-        const {data} = responseJSON;
+        const {data, pagination} = responseJSON;
         setPostList(data);
+        setPagination(pagination);
       } catch (error) {
         console.log('Error to fetch post lists: ', error.message);
       }
     }
-    console.log('POST list effect');
     fetchPostList();
-  }, []);
+  }, [filter]);
 
   useEffect(() => {
-    console.log('TODO list effect');
-  })
+
+  });
+
+  function handlePageChange(newPage) {
+    // console.log('newPage', newPage);
+    setFilter({
+      ...filter,
+      _page: newPage
+    });
+  }
 
   function handleTodoClick(todo) {
     const index = todoList.findIndex(x => x.id === todo.id);
@@ -54,6 +78,15 @@ function App() {
     setTodoList(newTodoList);
   }
 
+  function handleFilterChange(newFilter) {
+    // console.log(newFilter);
+    setFilter({
+      ...filter,
+      _page: 1,
+      title_like: newFilter.searchTerm
+    });
+  }
+
 
   return (
     <div className="app">
@@ -63,7 +96,12 @@ function App() {
       <TodoList todos={todoList} onTodoClick={handleTodoClick}/>
 
       <hr/>
+      <PostFilterForm onSubmit={handleFilterChange}/>
       <PostList posts={postList} />
+      <Pagination
+        pagination={pagination}
+        onPageChange={handlePageChange}
+      />
 
     </div>
   );
